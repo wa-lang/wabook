b434f8f448ffa1993dcc7fecee63e512e7178870
chai2010 <chaishushan@gmail.com>
2024-06-19 07:49:07 +0800

    zz

--- pkg/render/slide.go
+++ pkg/render/slide.go
@@ -11,7 +11,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/wa-lang/mnbook/pkg/mntalk/present"
+	"github.com/wa-lang/mnbook/pkg/present"
 )
 
 func (p *BookRendor) renderAllTalkPages() error {
